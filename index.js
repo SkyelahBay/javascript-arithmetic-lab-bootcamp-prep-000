0d9f11dd987f963083adad01cafb05cb9e9c0a7c
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-10 22:13:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ let mult = (a,b) => a*b;
 let div = (a,b) => a/b;
 let inc = (n) => n++;
 let dec = (n) => n--;
-let makeInt = (n) => parseInt(n);
+let makeInt = (n) => parseInt(n, 10);
+let preserveDecimal = (n) => parseFloat(n);
