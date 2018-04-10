0c45f4716aa7a7b15f546e229bb2068c57ce33d4
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-10 22:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 let add = (a,b) => a+b;
-
+let sub = (a,b) => a-b;
+let mult = (a,b) => a*b;
+let div = (a,b) => a/b;
+let inc = (n) => n++;
+let dec = (n) => n--;
+let makeInt = (n) => parseInt(n);
